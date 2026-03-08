8df380f4b5de6513ca575bfff917a47f90b832e3
mahlet <mahlet2ambaye@gmail.com>
2026-03-08 08:50:14 +0000

    fix : back with original code

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,4 @@ import "./styles/index.css";
 import React from "react";
 
 
-document.body.innerText = window.location.href;
-
-// if (window.location.pathname === "/debug") {
-//   document.body.innerText = window.location.href;
-// } else {
-//   createRoot(document.getElementById("root")!).render(<App />);
-// }
+createRoot(document.getElementById("root")!).render(<App />);
