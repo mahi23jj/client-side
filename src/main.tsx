561e2bfd9d4712f155d7b0a5ec31e1b887449019
kidist311 <alemayehukidist09@gmail.com>
2026-03-08 11:08:42 +0300

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 import React from "react";
+//import { BrowserRouter } from "react-router-dom";
 
+// This is a temporary solution to display the current URL in the body
 
 document.body.innerText = window.location.href;
 
